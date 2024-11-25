304bf56e04c1294efe2a36b5cfaf872c79a1189b
VietDucc <21521959@gm.uit.edu.vn>
2024-11-25 21:45:46 +0700

    5

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "http://localhost:3001";
+const SOCKET_SERVER_URL = "http://backend:3001";
 const socket = io(SOCKET_SERVER_URL);
 
 export default socket;
