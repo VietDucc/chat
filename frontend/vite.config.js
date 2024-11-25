6d888c1da36440969f72de414f31cb53d1776f79
VietDucc <21521959@gm.uit.edu.vn>
2024-11-25 21:29:30 +0700

    4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Lắng nghe trên tất cả các địa chỉ IP
+    port: 5173, // Bạn có thể giữ lại cổng 5173 hoặc thay đổi tùy theo nhu cầu
+  },
+});
