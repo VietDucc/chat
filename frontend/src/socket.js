03d30d4fe67e907e22cb60f4a06d6e80f201f5c9
VietDuc <21521959@gm.uit.edu.vn>
2025-02-09 22:40:50 +0700

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "http://192.168.1.40:3001";
-const socket = io(SOCKET_SERVER_URL);
+const SOCKET_SERVER_URL = "https://chat-woad-alpha.vercel.app";
+const socket = io(SOCKET_SERVER_URL, {
+  transports: ["websocket"], // Giúp tối ưu kết nối
+  withCredentials: true, // Nếu có sử dụng cookies hoặc xác thực
+});
 
 export default socket;
