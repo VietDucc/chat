baaff16b5d690094520f8cbe27919c903be4a900
VietDuc <21521959@gm.uit.edu.vn>
2025-02-09 23:05:15 +0700

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "https://chat-woad-alpha.vercel.app/api/socket"; // Kết nối đúng API route
+const SOCKET_SERVER_URL = "https://chat-gilt-nu.vercel.app/api/socket"; // Kết nối đúng API route
 
 const socket = io(SOCKET_SERVER_URL, {
   transports: ["websocket", "polling"], // Cần hỗ trợ polling
